0db259aea9de064097f891a71a15f6e0a277eca4
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2022-11-03 17:12:30 +0800

    initial import

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello, Express</h1>')
+})
+
+app.listen(3000, ()=>{
+    console.log('Application started on port 3000')
+})
